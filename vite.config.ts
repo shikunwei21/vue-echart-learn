f8af55ce9613750c0f6a03c2bcb11ccf797d8b40
Shi Kunwei <896466415@qq.com>
2023-03-03 23:38:14 +0800

    add plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),   Components({
+    resolvers: [ElementPlusResolver()],
+  }),
+],
 })
